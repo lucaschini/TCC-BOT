6b3b2a7a5050241b2dd1021e416e155f0c79fa9c
lul <83666061+lul0101@users.noreply.github.com>
2023-01-04 10:15:30 -0300

    Initial Bot complete + error on events

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // Require the necessary discord.js classes
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-// Log in to Discord with your client's token
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+
 client.login(token);
